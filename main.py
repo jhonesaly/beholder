0d70c63e54fbb96204f54455b4c69f2c2903e50f
Jhones <jhonesaly@gmail.com>
2023-11-20 11:25:24 -0300

    feat: remoção do tempo

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from bs4 import BeautifulSoup
 from win10toast import ToastNotifier
 import time
 
-def check_website_change(url, interval_minutes=1):
+def check_website_change(url):
     toaster = ToastNotifier()
     previous_content = None
+    i = 0
 
     while True:
+        i += 1
+        print(f"Observação {i}")
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -17,15 +20,18 @@ def check_website_change(url, interval_minutes=1):
 
             if previous_content is not None and current_content != previous_content:
                 toaster.show_toast("Mudança Detectada", "A página alvo foi alterada!")
-            
+
             previous_content = current_content
 
         except requests.exceptions.RequestException as e:
             print(f"Erro ao obter a página: {e}")
 
-        time.sleep(interval_minutes * 60)
+        time.sleep(10)
 
 if __name__ == "__main__":
-    # Substitua a URL pela página que você deseja monitorar
-    target_url = "https://exemplo.com"
-    check_website_change(target_url)
+    # Importa a URL do arquivo de configuração
+    with open('main.conf', 'r') as config_file:
+        config = config_file.read()
+        exec(config)
+
+    check_website_change(URL_ALVO)
