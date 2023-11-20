1bca13d074e78ebcf51a9906ead89efc85ca15d3
Jhones <jhonesaly@gmail.com>
2023-11-20 11:27:48 -0300

    feat: comparação

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ def check_website_change(url):
             current_content = str(soup)
 
             if previous_content is not None and current_content != previous_content:
-                toaster.show_toast("Mudança Detectada", "A página alvo foi alterada!")
+                changes = find_changes(previous_content, current_content)
+                if changes:
+                    toast_message = "Mudança Detectada:\n" + changes
+                    toaster.show_toast("Mudança Detectada", toast_message)
 
             previous_content = current_content
 
@@ -27,6 +30,14 @@ def check_website_change(url):
             print(f"Erro ao obter a página: {e}")
 
         time.sleep(10)
+
+def find_changes(previous_content, current_content):
+    # Encontre diferenças entre o conteúdo anterior e atual (pode precisar de uma biblioteca adicional)
+    # Neste exemplo, estamos apenas comparando as strings diretamente
+    if previous_content != current_content:
+        return "Conteúdo anterior:\n{}\n\nConteúdo atual:\n{}".format(previous_content, current_content)
+    else:
+        return None
 
 if __name__ == "__main__":
     # Importa a URL do arquivo de configuração
